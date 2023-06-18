80ad0d4c5582463852352479d0593e811d1c365e
ggujin <yang97816@gmail.com>
2023-06-18 17:58:17 +0900

    setup emotion tailwind css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,42 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react"
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document"
+import { extractCritical } from "@emotion/server"
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const page = await ctx.renderPage()
+    const initialProps = await Document.getInitialProps(ctx)
+    const critical = extractCritical(page.html)
+
+    return {
+      ...initialProps,
+      styles: (
+        <React.Fragment>
+          {initialProps.styles}
+          <style
+            data-emotion-css={critical.ids.join(" ")}
+            dangerouslySetInnerHTML={{ __html: critical.css }}
+          />
+        </React.Fragment>
+      ),
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
