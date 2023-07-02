66c019a02f9c4eecfdafb2705c2d18260ff37f4b
ggujin <yang97816@gmail.com>
2023-07-02 14:17:35 +0900

    chore: 테일윈드 config 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          light: "#1e293b",
+          main: "#0f172a",
+          dark: "#020617",
+        },
+        contents: {
+          light: "#ffffff",
+          main: "#f3f4f6",
+          dark: "#9ca3af",
+        },
       },
     },
   },
