0c3bb4afb1707a9ccbaeaf2bd5de3451135b2474
ggujin <yang97816@gmail.com>
2023-07-02 15:40:14 +0900

    style: 나눔고딕 폰트 추가

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,9 +1,12 @@
-import { Global, css } from "@emotion/react"
-import tw, { GlobalStyles as BaseStyles } from "twin.macro"
+import { Global, css } from '@emotion/react'
+import tw, { GlobalStyles as BaseStyles } from 'twin.macro'
+
+import { nanumGothic } from './font'
 
 const customStyles = css({
   body: {
     ...tw`antialiased text-contents-main`,
+    ...nanumGothic.style,
   },
 })
 
---
+++ src/styles/font.ts
@@ -0,0 +1,8 @@
+import { Nanum_Gothic } from 'next/font/google'
+
+export const nanumGothic = Nanum_Gothic({ weight: '400', subsets: ['latin'] })
+
+export const nanumGothicBold = Nanum_Gothic({
+  weight: '700',
+  subsets: ['latin'],
+})
