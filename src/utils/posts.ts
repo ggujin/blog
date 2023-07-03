804dda49ae7a417267707a48a9847035e6b0ada2
ggujin <yang97816@gmail.com>
2023-07-03 22:50:30 +0900

    feat: posts 유틸 파일 추가

---
+++ src/utils/posts.ts
@@ -0,0 +1,45 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+interface Frontmatter {
+  emoji: string
+  title: string
+  createdAt: Date
+  tags: string[]
+}
+
+interface Post extends Frontmatter {
+  slug: string
+  content: string
+}
+
+export function getPosts() {
+  const dirFiles = fs.readdirSync(path.join(process.cwd(), 'posts'), {
+    withFileTypes: true,
+  })
+
+  const posts: Post[] = dirFiles
+    .filter(file => file.name.endsWith('.mdx'))
+    .map(post => getPost(post.name))
+
+  return posts
+}
+
+export function getPostBySlug(slug: string) {
+  return getPost(`${slug}.mdx`)
+}
+
+function getPost(filename: string): Post {
+  const fileContent = fs.readFileSync(
+    path.join(process.cwd(), 'posts', filename),
+    'utf-8',
+  )
+  const { data, content } = matter(fileContent)
+
+  return {
+    ...(data as Frontmatter),
+    content,
+    slug: filename.slice(filename.lastIndexOf('.')),
+  }
+}
