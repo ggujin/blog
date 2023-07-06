8823be6bbffb9d75543383536d7864530fbfa155
ggujin <yang97816@gmail.com>
2023-07-06 21:09:57 +0900

    feat: DetailPage

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,22 +1,30 @@
 import type { NextPage, GetStaticPaths, GetStaticProps } from 'next/'
-import { getPosts, getPostBySlug } from '@/utils/posts'
-
-import { PostItem } from '@/types'
 import { format } from 'date-fns'
+
+import { getPosts, getPostBySlug } from '@/utils/posts'
+import { Layout } from '@/components'
+import { PostItem } from '@/types'
 
 interface PageProps {
   post: PostItem
 }
 
 const PostDetailPage: NextPage<PageProps> = ({ post }) => {
+  const { title, content, emoji, createdAt } = post ?? {}
   // 여기서 에러나면 브라우저 콘솔
   return (
-    <div>
-      <h1>{post?.title}</h1>
-    </div>
+    <Layout>
+      <h1>
+        {emoji}
+        {title}
+      </h1>
+      <div dangerouslySetInnerHTML={{ __html: content ?? '' }} />
+      <p>{createdAt}</p>
+    </Layout>
   )
 }
 
+// 다이나믹 세그먼트에 들어갈 값을 미리 만듬 동적 경로
 export const getStaticPaths: GetStaticPaths = () => {
   //여기서 에러나면 서버사이드이기때문에 터미널을 본다.
   return {
